b31756650beb4eb95e31bb4b667d61aac37b1467
arqdsis <arqdsis@mocli10225.animaedu.intranet>
2018-05-02 20:06:05 -0300

    MVC

---
+++ app/src/test/java/devmob/pedrogallon/calculadoramvc/TestCalculadora.java
@@ -0,0 +1,50 @@
+package devmob.pedrogallon.calculadoramvc;
+
+import org.junit.Test;
+
+
+import devmob.pedrogallon.calculadoramvc.model.Calculadora;
+
+import static junit.framework.Assert.assertEquals;
+
+public class TestCalculadora {
+    @Test
+    public void testaCalculadora(){
+        Calculadora model = new Calculadora();
+        model.setValor1("2");
+        model.setValor2("2");
+        model.setOperador("+");
+        model.calculo();
+        assertEquals("4", model.getResultado());
+        model.setOperador("-");
+        model.calculo();
+        assertEquals("0", model.getResultado());
+        model.setOperador("*");
+        model.calculo();
+        assertEquals("4", model.getResultado());
+        model.setOperador("/");
+        model.calculo();
+        assertEquals("1", model.getResultado());
+
+        model.setValor1("3");
+        model.setValor2("2");
+        model.setOperador("+");
+        model.calculo();
+        assertEquals("5", model.getResultado());
+        model.setOperador("-");
+        model.calculo();
+        assertEquals("1", model.getResultado());
+        model.setOperador("*");
+        model.calculo();
+        assertEquals("6", model.getResultado());
+        model.setOperador("/");
+        model.calculo();
+        assertEquals("1", model.getResultado());
+        model.setValor1("2");
+        model.setValor2("3");
+        model.setOperador("/");
+        model.calculo();
+        assertEquals("0", model.getResultado());
+
+    }
+}
